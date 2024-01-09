50b8877672243cf9022e5c4d2a77e974496cc738
Nguyen Anh Tu <kinganhtu09022001@gmail.com>
2024-01-09 14:22:13 +0700

    create model brand, product

---
+++ app/Models/Product.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Product extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $table = 'products';
+
+    protected $fillable = [
+        'brand_id',
+        'name',
+        'description',
+        'price',
+        'qty',
+        'discount',
+        'size',
+        'tags',
+        'featured',
+    ];
+
+    public function brand()
+    {
+        return $this->belongsTo(Brand::class, 'brand_id', 'id');
+    }
+}
