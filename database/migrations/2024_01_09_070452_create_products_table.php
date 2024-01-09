08061d9794a28956098eff984ba24be19f1a69bf
Nguyen Anh Tu <kinganhtu09022001@gmail.com>
2024-01-09 14:13:26 +0700

    create migration

---
+++ database/migrations/2024_01_09_070452_create_products_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->integer('brand_id');
+            $table->string('name')->nullable();
+            $table->text('description')->nullable();
+            $table->string('price')->nullable();
+            $table->integer('qty');
+            $table->string('discount')->nullable();
+            $table->string('size')->nullable();
+            $table->string('tags');
+            $table->boolean('featured');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
