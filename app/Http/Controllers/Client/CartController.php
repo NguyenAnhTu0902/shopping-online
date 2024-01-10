b7c4ad4b6ea5dc341932555a4eaace34f0c8488f
Nguyen Anh Tu <kinganhtu09022001@gmail.com>
2024-01-10 16:37:28 +0700

    add product to cart by session, show product in cart

--- app/Http/Controllers/Client/CartController.php
+++ app/Http/Controllers/Client/CartController.php
@@ -18,31 +18,49 @@ class CartController extends Controller
 
     public function index()
     {
-
+        $products = $this->productService->all();
+        $carts = Cart::content();
+        $total = Cart::total();
+        $subtotal = Cart::subtotal();
+        return view('layouts.client.page.cart', compact('products', 'carts', 'total', 'subtotal'));
     }
     public function addCart(Request $request)
     {
         if ($request->ajax()) {
             $product = $this->productService->find($request->productId);
-            $response['cart'] = Cart::add([
-                'id' => $product->id,
-                'name' => $product->name,
-                'qty' => 1,
-                'price' => $product->discount ?? $product->price,
-                'weight' => $request->size,
-                'image' => $product->productImages,
-                'options' => [
-                    'images' => $product->productImages[0]->path,
-                ],
-            ]);
+            $carts = Cart::content();
+            foreach ($carts as $cart)
+            {
+                if($cart->id == $request->productId){
+                    $existingItem = Cart::get($cart->rowId, ['weight' => $request->size]);
+                }
+            }
+            if (!$existingItem){
+                $response['cart'] = Cart::add([
+                    'id' => $product->id,
+                    'name' => $product->name,
+                    'qty' => 1,
+                    'price' => $product->discount ?? $product->price,
+                    'weight' => $request->size,
+                    'image' => $product->productImages,
+                    'rowId' => rand()
+                ]);
+            }
+            else
+            {
+                $response['cart'] = Cart::add([
+                    'id' => $product->id,
+                    'name' => $product->name,
+                    'qty' => 1,
+                    'price' => $product->discount ?? $product->price,
+                    'weight' => $request->size,
+                    'image' => $product->productImages,
+                ]);
+            }
             $response['count'] = Cart::count();
             $response['total'] = Cart::total();
-
             return $response;
         }
-
-        return back();
-
         return back();
     }
 }
--- resources/views/layouts/client/page/cart.blade.php
+++ resources/views/layouts/client/page/cart.blade.php
@@ -12,6 +12,7 @@
                             <tr>
                                 <th>Image</th>
                                 <th class="p-name">Product Name</th>
+                                <th>Size</th>
                                 <th>Price</th>
                                 <th>Quantity</th>
                                 <th>Total</th>
@@ -19,38 +20,46 @@
                             </tr>
                             </thead>
                             <tbody>
-                                <tr>
-                                    <td class="cart-pic first-row"><img src="front/img/products/loafer-1.png" alt=""></td>
+                            @foreach($carts as $cart)
+                                <tr data-rowId="{{$cart->rowId}}">
+                                    @foreach($products as $product)
+                                        @if($cart->id == $product -> id )
+                                            <td class="cart-pic first-row"><img src="front/img/products/{{$product->productImages[0]->path}}" alt=""></td>
+                                        @endif
+                                    @endforeach
                                     <td class="cart-title first-row">
-                                        <h5>Loafer S202</h5>
+                                        <h5>{{$cart->name}}</h5>
                                     </td>
-                                    <td class="p-price first-row">$20</td>
+                                        <td class="p-price first-row">{{$cart->weight}}</td>
+                                    <td class="p-price first-row">${{number_format($cart->price,2)}}</td>
                                     <td class="qua-col first-row">
                                         <div class="quantity">
-                                            <input class="pro-qty" value="5" disabled>
+                                            <div class="pro-qty">
+                                                <input disabled="disabled" type="number" value="{{$cart -> qty}}" data-rowid="{{$cart->rowId}}" min="0">
+                                            </div>
                                         </div>
                                     </td>
-                                    <td class="total-price first-row">$100</td>
+                                    <td class="total-price first-row">${{number_format($cart->price * $cart->qty, 2)}}</td>
                                     <td class="close-td first-row">
-                                        <i onclick="" class="ti-close"></i>
+                                        <i onclick="removeCart('{{$cart->rowId}}')" class="ti-close"></i>
                                     </td>
                                 </tr>
+                            @endforeach
                             </tbody>
                         </table>
                     </div>
                     <div class="row">
                         <div class="col-lg-4">
                             <div class="cart-buttons">
-                                <a href="#" class="primary-btn continue-shop">Continue shopping</a>
                                 <a href="/san-pham" class="primary-btn up-cart">Update cart</a>
                             </div>
                         </div>
                         <div class="col-lg-4 offset-lg-4">
                             <div class="proceed-checkout">
                                 <ul>
-                                    <li class="cart-total">Total <span>$100</span></li>
+                                    <li class="cart-total">Total <span>${{Cart::total()}}</span></li>
                                 </ul>
-                                <a href="" class="proceed-btn">PROCEED TO CHECK OUT</a>
+                                <a href="/order" class="proceed-btn">PROCEED TO CHECK OUT</a>
                             </div>
                         </div>
                     </div>
@@ -59,4 +68,6 @@
         </div>
     </div>
     <!-- Shopping Cart Section End -->
+
+
 @endsection
