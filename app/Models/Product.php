a05d45b6a3422abdb1f08e8c6935ef850cb208da
Nguyen Anh Tu <kinganhtu09022001@gmail.com>
2024-01-12 09:41:41 +0700

    add categories table and update foreigh key

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -14,16 +14,20 @@ class Product extends Model
     protected $table = 'products';
 
     protected $fillable = [
+        'category_id',
         'brand_id',
         'name',
         'description',
         'price',
         'qty',
         'discount',
-        'size',
-        'tags',
         'featured',
     ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'category_id', 'id');
+    }
 
     public function brand()
     {
